b3c6a0aa408e413522bb9c115cdba411921244ca
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-08-16 20:13:24 +0600

    Date object and date methods

---
+++ 02 - Data Structures/09 - Date object and date methods/index.js
@@ -0,0 +1,18 @@
+let date = new Date();
+
+console.log(date);
+
+let currentDate = date.getDate();
+console.log(currentDate);
+
+let currentYear = date.getFullYear();
+console.log(currentYear);
+
+let currentTime = date.getTime();
+console.log(currentTime);
+
+let currentMinute = date.getMinutes();
+console.log(currentMinute);
+
+let currentSecond = date.getSeconds();
+console.log(currentSecond);
