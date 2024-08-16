0205ef219c1dde21ef89d3049f9487d4c08051b7
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-08-16 11:42:57 +0600

    How to create and use array

---
+++ 02 - Data Structures/01 - How to create and use array/index.js
@@ -0,0 +1,14 @@
+let names = ["John", "Wick", "Leon", "Tom", "David"];
+console.log(names);
+console.log(names[2]);
+names.push("Roman");
+console.log(names);
+names.pop();
+console.log(names);
+
+
+let country1 = ["Bangladesh", "Pakistan"];
+let country2 = ["India", "Nepal"];
+
+let countries = country1.concat(country2);
+console.log(countries);
