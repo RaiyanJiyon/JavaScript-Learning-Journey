a222025b73597ca9c05bd9301681f8062f4c8410
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-08-16 19:19:30 +0600

    Math Object

---
+++ 02 - Data Structures/07 - Math Object/index.js
@@ -0,0 +1,12 @@
+console.log(Math.floor(1.4));
+console.log(Math.ceil(1.4));
+console.log(Math.abs(-1));
+console.log(Math.sin(2));
+console.log(Math.cos(2));
+console.log(Math.random());
+console.log(Math.random() * 5);
+console.log((Math.random() * 5 ) + 10 );
+console.log(Math.max(1, 3, 5, 11));
+console.log(Math.min(1, 3, 5, 11));
+console.log(Math.round(1.4));
+console.log(Math.pow(2, 2));
