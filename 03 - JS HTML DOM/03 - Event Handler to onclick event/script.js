58c53fac285c8b438ea67578215d6230f3cf989c
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-08-18 10:40:59 +0600

    Event Handler to onclick event

---
+++ 03 - JS HTML DOM/03 - Event Handler to onclick event/script.js
@@ -0,0 +1,17 @@
+function display1() {
+    // alert('Hello World!')
+    document.querySelector("#pid").innerHTML  = "Hello World!";
+}
+
+function display2() {
+    // alert('Hello JavaScript!')
+    document.querySelector("#pid").innerHTML  = "Hello JavaScript!";
+}
+
+function image1() {
+    document.querySelector("#imageid").src = "pic1.jpg";
+}
+
+function image2() {
+    document.querySelector("#imageid").src = "pic2.jpg";
+}
